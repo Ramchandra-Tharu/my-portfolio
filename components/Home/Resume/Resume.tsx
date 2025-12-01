494f9d7e4529fbf5b90d263503a5a3390873a7d9
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-01 23:36:50 +0530

    updates on resume section

--- components/Home/Resume/Resume.tsx
+++ components/Home/Resume/Resume.tsx
@@ -7,18 +7,15 @@ const ResumeSection = () => {
         <div className='pt-20 pb-20'>
             <div className="w-[90%] sm:w-[70%] mx-auto grid grid-flow-col auto-cols-[250px] gap-10 text-white overflow-x-auto py-10">
                 {/* work */}
-                <div className='text-2xl font-bold text-white'>
-                    <h1> My  <span className='text-cyan-400'>Work Experience</span></h1>
+                 <div className='text-2xl font-bold text-white'>
+                    <h1> My  <span className='text-cyan-400'>Education</span></h1>
+                    <ResumeCard Icon={FaCodepen} role="Full Stack Devloper" />
+                    <ResumeCard Icon={FaReact} role="Full Stack Devloper" />
+                    <ResumeCard Icon={FaDatabase} role="Full Stack Devloper" />
 
                 </div>
                 {/* education*/}
-                <div className='text-2xl font-bold text-white'>
-                    <h1> My  <span className='text-cyan-400'>Education</span></h1>
-                    <ResumeCard icon={FaCodepen} role="Full Stack Devloper" />
-                    <ResumeCard icon={FaReact} role="Full Stack Devloper" />
-                    <ResumeCard icon={FaDatabase} role="Full Stack Devloper" />
-
-                </div>
+               
                 <div>
                 </div>
             </div>
--- components/Home/Resume/ResumeCard.tsx
+++ components/Home/Resume/ResumeCard.tsx
@@ -1,9 +1,33 @@
+import { Icon } from 'next/dist/lib/metadata/types/metadata-types'
 import React from 'react'
+import { IconType } from 'react-icons';
 
-const ResumeCard = () => {
+type Props = {
+    Icon: IconType;
+    role:string;
+
+
+}
+
+const ResumeCard = ({Icon, role}: Props) => {
   return (
-    <div>ResumeCard</div>
+    <div className='mb-6'>
+        <div className="flex items-start  space-x-6 bg-blue-900 transition-all duration-300 sm:p-8 rounded-md">
+        <div className='sm:h-14 sm:w-14 h-4 w-4 bg-blue-950 rounded-full flex items-center justify-center flex-col'>
+            <Icon className = " sm:w-8 sm:-8 h-6 w-6 text-white"/>
+            
+        </div>
+       <div className='flex-1'>
+        <h1 className='text-gray text-xl font-semibold'>
+            {role}
+        </h1>
+        </div>
+        
+
+        </div>
+
+    </div>
   )
 }
 
-export default ResumeCard
+export default ResumeCard;
