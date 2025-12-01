61b4bcb69ed0325eee6c8d8067f7539a38b3aa50
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-01 22:58:00 +0530

    resume section added

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { HeroSection } from './Hero';
 import ServicesSection from './Service/Services';
+import ResumeSection from './Resume/Resume';
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <HeroSection />
       <ServicesSection />
+      <ResumeSection />
       
 
     </div>
--- components/Home/Service/Services.tsx
+++ components/Home/Service/Services.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import ServiceCard from './ServiceCard';
 
 const ServicesSection = () => {
+
     return (
         <div className='pt-16 pb-16'>
             <div className='text-white font-bold text-2xl md:text-4xl text-center'>
                 Quality services for your needs. <br />
                 Fast, clean and reliable work.
             </div>
+            <div className="w-[90%] sm:w-[70%] mx-auto grid gap-6 mt-20 
+                grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+
+
+
+                <ServiceCard
+                    icon="/images/shape.png"
+                    name=" UI and UX"
+                    description=" Designing how a product looks and feels to create a smooth, enjoyable user experience."
+                />
+                <ServiceCard
+                    icon="/images/s2.png"
+                    name=" UI and UX"
+                    description=" Designing how a product looks and feels to create a smooth, enjoyable user experience."
+                />
+                <ServiceCard
+                    icon="/images/s3.png"
+                    name=" UI and UX"
+                    description=" Designing how a product looks and feels to create a smooth, enjoyable user experience."
+                />
+                <ServiceCard
+                    icon="/images/s4.png"
+                    name=" UI and UX"
+                    description=" Designing how a product looks and feels to create a smooth, enjoyable user experience."
+                />
+
+            </div>
+
         </div>
     )
 }
