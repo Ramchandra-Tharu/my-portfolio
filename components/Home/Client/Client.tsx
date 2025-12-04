7b306a9573a29d232d64be7c5fb7b73b509108a1
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-05 00:58:32 +0530

    new features on client review system

---
+++ components/Home/Client/Client.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ClientSection = () => {
+  return (
+    <div className='pb-16 pt-16'>
+        <h1 className='text-center text-2xl font-bold text-white'>
+            Kind words from satisfied <br /> 
+            <span className='text-cyan-200'>Clients</span>
+        </h1>
+        <div className='mt-16 w-[70%] max-auto'>
+            
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ClientSection;
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,6 +4,7 @@ import ServicesSection from './Service/Services';
 import ResumeSection from './Resume/Resume';
 import ProjectSection from './Project/Project';
 import SkillSection from './Skill/Skill';
+import ClientSection from './Client/Client';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <ResumeSection />
       <ProjectSection />
       <SkillSection />
+      <ClientSection />
 
       
 
