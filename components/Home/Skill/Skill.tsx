e1f3fa696895dcfa22bcbbbb5b345ef0d58cc179
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-04 22:42:28 +0530

    skill section

--- components/Home/Skill/Skill.tsx
+++ components/Home/Skill/Skill.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import {
+     
+  SiJavascript, 
+  SiNextdotjs, 
+  SiReact, 
+  SiFlutter 
+} from "react-icons/si";
 
 const SkillSection = () => {
+    const skills = [
+
+  { 
+    name: "JavaScript", 
+    logo: SiJavascript,
+    percent: 60 
+  },
+  { 
+    name: "Next.js", 
+    logo: SiNextdotjs,
+    percent: 60 
+  },
+  { 
+    name: "React", 
+    logo: SiReact,
+    percent: 60 
+  },
+  { 
+    name: "Flutter", 
+    logo: SiFlutter,
+    percent: 60 
+  },
+];
+
+    
   return (
-    <div className='text-white'>Section</div>
+    <div className='text-white pt-14 pb-16'>
+<h1 className='text-white text-bold text-center text-2xl sm:text-4xl xl:text-5xl font-bold'>
+    My <span className='text-cyan-200'> Skills</span>    
+</h1>
+    </div>
   )
 }
 
