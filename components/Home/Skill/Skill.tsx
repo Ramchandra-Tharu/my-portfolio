39a1cd8f6c80dc4b0d7e02e1b71756a2ebaa4584
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-04 23:58:29 +0530

    skill section compelete

--- components/Home/Skill/Skill.tsx
+++ components/Home/Skill/Skill.tsx
@@ -1,43 +1,86 @@
+"use client";
+import { Scada } from 'next/font/google';
 import React from 'react'
 import {
-     
-  SiJavascript, 
-  SiNextdotjs, 
-  SiReact, 
-  SiFlutter 
+
+  SiJavascript,
+  SiOpenjdk,
+  SiNextdotjs,
+  SiReact,
+  SiFlutter,
+  SiPycharm,
+  SiHtml5
 } from "react-icons/si";
+import Tilt from 'react-parallax-tilt'
 
 const SkillSection = () => {
-    const skills = [
+  const skills = [
 
-  { 
-    name: "JavaScript", 
-    logo: SiJavascript,
-    percent: 60 
-  },
-  { 
-    name: "Next.js", 
-    logo: SiNextdotjs,
-    percent: 60 
-  },
-  { 
-    name: "React", 
-    logo: SiReact,
-    percent: 60 
-  },
-  { 
-    name: "Flutter", 
-    logo: SiFlutter,
-    percent: 60 
-  },
-];
-
+    {
+      name: "JavaScript",
+      logo: SiJavascript,
+      percent: 60
+    },
     
+    {
+      name : "Html",
+      logo: SiHtml5,
+      percent :90
+    },
+  
+    
+    {
+      name: "Java",
+      logo: SiOpenjdk,
+      percent: 60
+    },
+    {
+      name: "Next.js",
+      logo: SiNextdotjs,
+      percent: 60
+    },
+    {
+      name: "React",
+      logo: SiReact,
+      percent: 60
+    },
+    {
+      name: "Flutter",
+      logo: SiFlutter,
+      percent: 60
+    },
+  ];
+
+
   return (
     <div className='text-white pt-14 pb-16'>
-<h1 className='text-white text-bold text-center text-2xl sm:text-4xl xl:text-5xl font-bold'>
-    My <span className='text-cyan-200'> Skills</span>    
-</h1>
+      <h1 className='text-white text-bold text-center text-2xl sm:text-4xl xl:text-5xl font-bold'>
+        My <span className='text-cyan-200'> Skills</span>
+      </h1>
+      <div className='justify-center mt-16  gap-6 flex flex-wrap'>
+        {
+  skills.map((skill) => {
+    return (
+      <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+        <div className="bg-[#14134145] w-40 h-48 text-center rounded-3xl flex flex-col items-center justify-center shadow-lg transition scale-105">
+          
+          <div className="text-5xl mb-4 text-gray-300">
+            <skill.logo size={50} />
+          </div>
+
+          <p className="text-2xl font-semibold">{skill.percent}%</p>
+          <p className="text-purple-300 mt-1">{skill.name}</p>
+
+        </div>
+      </Tilt>
+    );
+  })
+}
+
+
+
+
+      </div>
     </div>
   )
 }
