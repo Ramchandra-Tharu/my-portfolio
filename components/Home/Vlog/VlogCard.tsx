3de0366013677fe0ef9f5af1eaa5c5a48396b544
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-11 22:55:40 +0530

    contact section is created

--- components/Home/Vlog/VlogCard.tsx
+++ components/Home/Vlog/VlogCard.tsx
@@ -17,11 +17,11 @@ const VlogCard = ({ image, title }: Props) => {
             <p className='mt-4 text-gray-500 font-medium text-base sm:text-lg'>24 Sept 2025</p>
             <h2 className='text-white mt-5 text-sm font-bold sm:text-base hover:underline hover:text-cyan-200 transition-all duration-300'>
                 {title}
-                </h2>
-                <div className="mt-4 gap-4 flex items-center">
-                    <p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>React</p>
-<p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>Nextjs</p>
-<p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>Tailwind</p>                </div>
+            </h2>
+            <div className="mt-4 gap-4 flex items-center">
+                <p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>React</p>
+                <p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>Nextjs</p>
+                <p className='px-4 py-1.5  bg-blue-950 text-sm text-white sm:text-sm font-bold rounded-full'>Tailwind</p>                </div>
         </div>
     )
 }
